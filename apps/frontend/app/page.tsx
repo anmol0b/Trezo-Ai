94aadc1fbcf4416771655f20a557c44d6e7b9b4c
Just_Anniee <145839789+Annieeeee11@users.noreply.github.com>
2026-05-10 11:31:52 +0530

    feat: fixed page component

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,30 +1,29 @@
-import Image, { type ImageProps } from "next/image";
+// import Image, { type ImageProps } from "next/image";
 import "./globals.css"
-import Hero from "../components/hero";
 import Landing from "../pages/landing";
-import { HeroSection } from "../components/coming-soon/hero";
+// import { HeroSection } from "../components/coming-soon/hero";
 
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
+// type Props = Omit<ImageProps, "src"> & {
+//   srcLight: string;
+//   srcDark: string;
+// };
 
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
+// const ThemeImage = (props: Props) => {
+//   const { srcLight, srcDark, ...rest } = props;
 
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
+//   return (
+//     <>
+//       <Image {...rest} src={srcLight} className="imgLight" />
+//       <Image {...rest} src={srcDark} className="imgDark" />
+//     </>
+//   );
+// };
 
 export default function Home() {
   return (
     <div>
-      {/* <Landing /> */}
-      <HeroSection />
+      <Landing />
+      {/* <HeroSection /> */}
     </div>
   );
 }
